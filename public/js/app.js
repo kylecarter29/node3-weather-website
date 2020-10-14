8fa90ffc46b3434cc4d93fe97295da14196848d3
Kyle Carter <kyc@trustpilot.com>
2020-10-14 15:16:54 -0600

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading...'
     messageTwo.textContent = ''
 
-    fetch('http://localhost:3000/weather?address=' + location).then((response) =>{ // fetch API call
+    fetch('/weather?address=' + location).then((response) =>{ // fetch API call
     response.json().then((data) => { // parse to Javascript
       if (data.error) {
         //   console.log (data.error) //handle error
